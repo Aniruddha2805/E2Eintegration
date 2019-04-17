f94c1f95782ee2c6442a1072e67bbb8ecae6fec4
admin <admin@user>
2019-04-17 15:18:50 +0530

    Commit megs

---
+++ seleniumTest/src/seleniumTest/Test1.java
@@ -0,0 +1,18 @@
+package seleniumTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test
+	public void test_002() {
+		System.out.println("This is my first jenkin Test");
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div/div[1]/div/div[1]/input")).sendKeys("Aniruddha");
+	}
+	
+}
